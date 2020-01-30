3b8cea5f3f3c3f658392ed688d14041ebd36d2c1
Leo Pan <pzf0000@foxmail.com>
2020-01-30 23:28:36 +0800

    Add files via upload

---
+++ test.cpp
@@ -0,0 +1,33 @@
+# define e while
+# define ee true
+# define eee using
+# define eeee namespace 
+# define eeeee std
+# define eeeeee ;
+# define eeeeeee int
+# define eeeeeeee main
+# define eeeeeeeee (
+# define eeeeeeeeee )
+# define eeeeeeeeeee {
+# define eeeeeeeeeeee }
+# define eeeeeeeeeeeee cout
+# define eeeeeeeeeeeeee <<
+# define eeeeeeeeeeeeeee 'e'
+# define eeeeeeeeeeeeeeee return
+# define eeeeeeeeeeeeeeeee 0
+# define eeeeeeeeeeeeeeeeee 100000
+# define eeeeeeeeeeeeeeeeeee t
+# define eeeeeeeeeeeeeeeeeeee =
+# define eeeeeeeeeeeeeeeeeeeee --
+
+#include <iostream>
+eee eeee eeeee eeeeee
+eeeeeee eeeeeeee eeeeeeeee eeeeeeeeee
+eeeeeeeeeee
+eeeeeee eeeeeeeeeeeeeeeeeee eeeeeeeeeeeeeeeeeeee eeeeeeeeeeeeeeeeee eeeeee
+e eeeeeeeee eeeeeeeeeeeeeeeeeee eeeeeeeeeeeeeeeeeeeee eeeeeeeeee
+eeeeeeeeeee
+eeeeeeeeeeeee eeeeeeeeeeeeee eeeeeeeeeeeeeee eeeeee
+eeeeeeeeeeee
+eeeeeeeeeeeeeeee eeeeeeeeeeeeeeeee eeeeee
+eeeeeeeeeeee
